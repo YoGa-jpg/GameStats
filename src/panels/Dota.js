73682c29e6245153cf41be1440f586306e5e11ba
YoGa-jpg <antondavidovich45@gmail.com>
2022-02-21 00:31:42 +0300

    Add tabs menu template

--- src/panels/Dota.js
+++ src/panels/Dota.js
@@ -2,18 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Panel, PanelHeader, PanelHeaderButton } from '@vkontakte/vkui';
+import { Tabbar, TabbarItem} from '@vkontakte/vkui'
 import { platform, IOS } from '@vkontakte/vkui';
-import { Icon28ChevronBack, Icon24Back } from '@vkontakte/icons';
+import { Icon28ChevronBack, Icon24Back} from '@vkontakte/icons';
+import { Icon28Profile } from '@vkontakte/icons';
+import { Icon28Newsfeed } from '@vkontakte/icons';
+import { Icon28CupOutline } from '@vkontakte/icons';
 
 import '../styles/Menu.css';
 
 const osName = platform();
 
-const Dota = ({ id, go}) => (
+const Dota = ({ id, go }) => (
 	<Panel id={id}>
 		<PanelHeader left={<PanelHeaderButton onClick={go} data-to="menu">
-            {osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
-        </PanelHeaderButton>}>DotA 2</PanelHeader>
+			{osName === IOS ? <Icon28ChevronBack /> : <Icon24Back />}
+		</PanelHeaderButton>}>DotA 2</PanelHeader>
+		<Tabbar>
+			<TabbarItem text="Новости">
+				<Icon28Newsfeed />
+			</TabbarItem>
+			<TabbarItem
+				text="Турниры"
+			>
+				<Icon28CupOutline />
+			</TabbarItem>
+			<TabbarItem
+				text="Профиль"
+			>
+				<Icon28Profile/>
+			</TabbarItem>
+		</Tabbar>
 	</Panel>
 );
 
